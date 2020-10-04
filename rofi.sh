cdd46ce7e2bd06a3b207827d183533e668042915
ryeenii <gustarrini@pm.me>
2020-10-04 19:52:37 -0300

    push rofi changes to master

--- rofi.sh
+++ rofi.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
-. "${HOME}/.cache/wal/colors.sh"
-rofi -show drun -width 99 -font "Hyperspace Race 10" -bw 2 -location 5 -yoffset -38 -xoffset -8 -icon-theme "Papirus-dark" -terminal urxvt -show-icons -drun-display-format "{name}"
-
+# !/bin/bash
+if [ -z $1 ]; then
+	rofi -show drun -width 15 -font "Montserrat Alternates Medium 10" -bw 2 -location 5 -yoffset -38 -xoffset -8 -icon-theme "Papirus-dark" -terminal st -show-icons -drun-display-format "{name}"
+else
+	rofi -show drun -width 15 -font "Montserrat Alternates Medium 10" -bw 2 -location 1 -yoffset $2 -xoffset $1 -icon-theme "Papirus-dark" -terminal st -show-icons -drun-display-format "{name}"
+fi
