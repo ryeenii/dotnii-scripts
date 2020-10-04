73bc7ba98687fc1a430adb31861c5f91a404b7f7
ryeenii <gustarrini@pm.me>
2020-10-04 19:52:03 -0300

    ensure that the python module version of streamlink can be also used

--- str.sh
+++ str.sh
@@ -2,23 +2,32 @@
 if [[ $1 = "-lr" || $1 = "-le" ]]; then
 	echo "Please insert the link first, then the flags."
 else
+	function binary() {
+		if ! which streamlink > /dev/null; then
+			echo "Streamlink binary not found, attempting python module..."
+			s="python3 -m streamlink"
+		else
+			s="streamlink"
+		fi
+	}
+	binary
 	if [ -z $2 ]; then
-		streamlink "$1" best --player mpv
+		${s} "$1" best --player mpv
 	else
 		if [[ $2 = "-lr" ]]; then
             if [[ $3 = "-o" ]]; then
-	            streamlink "${1}" best --hls-live-restart -o "rec-$(date +%d%m%y-%H%M%S).ts"
+	            ${s} "${1}" best --hls-live-restart -o "rec-$(date +%d%m%y-%H%M%S).ts"
             else
-	            streamlink "${1}" best --hls-live-restart --player mpv
+	            ${s} "${1}" best --hls-live-restart --player mpv
             fi
 		elif [[ $2 = "-le" ]]; then
             if [[ $4 = "-o" ]]; then
-	            streamlink "${1}" best --hls-live-edge "${3}" -o "rec-$(date +%d%m%y-%H%M%S).ts"
+	            $s "${1}" best --hls-live-edge "${3}" -o "rec-$(date +%d%m%y-%H%M%S).ts"
             else
-	            streamlink "${1}" best --hls-live-edge "${3}" --player mpv
+	            $s "${1}" best --hls-live-edge "${3}" --player mpv
             fi
 		elif [[ $2 = "-o" ]]; then
-	    		streamlink "${1}" best -o "rec-$(date +%d%m%y-%H%M%S).ts"
+	    		$s "${1}" best -o "rec-$(date +%d%m%y-%H%M%S).ts"
 		fi	
 	fi
 fi
