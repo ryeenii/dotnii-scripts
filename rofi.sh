76a21642fa7e55dd2a0047470735d8059add90cd
ryeenii <gustarrini@pm.me>
2020-10-04 20:03:45 -0300

    you weren't supposed to be there anymore :eyes:

--- rofi.sh
+++ rofi.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 if [ -z $1 ]; then
-	rofi -show drun -width 15 -font "Montserrat Alternates Medium 10" -bw 2 -location 5 -yoffset -38 -xoffset -8 -icon-theme "Papirus-dark" -terminal st -show-icons -drun-display-format "{name}"
+	rofi -show drun -width 15 -bw 2 -location 5 -yoffset -38 -xoffset -8 -icon-theme "Papirus-dark" -terminal st -show-icons -drun-display-format "{name}"
 else
-	rofi -show drun -width 15 -font "Montserrat Alternates Medium 10" -bw 2 -location 1 -yoffset $2 -xoffset $1 -icon-theme "Papirus-dark" -terminal st -show-icons -drun-display-format "{name}"
+	rofi -show drun -width 15 -bw 2 -location 1 -yoffset $2 -xoffset $1 -icon-theme "Papirus-dark" -terminal st -show-icons -drun-display-format "{name}"
 fi
